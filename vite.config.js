0eaba44e2dbd7a5bb41813e7b6c39ff9c94a1aeb
monuwebdev <mranjansingh07@gmail.com>
2025-08-19 10:29:08 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Ensure proper build for Vercel (ESM mode)
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 })
